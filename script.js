8d08c2d9ae44967a3fa2b8740a54b3f5594dd638
Nguyễn Thanh Lâm <49803180+lamdaik1412@users.noreply.github.com>
2025-01-23 14:27:07 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,112 +1,142 @@
-const randomButton = document.getElementById('randomButton');
-const resultDisplay = document.getElementById('result');
-const randomDenominationsDisplay = document.getElementById('randomDenominations');
- const countdownDisplay = document.getElementById('countdown');
-const apiKey = 'ab2c527a-02d5-4e27-aa08-698703c60474';
+const randomButton = document.getElementById("randomButton");
+const resultDisplay = document.getElementById("result");
+const randomDenominationsDisplay = document.getElementById(
+  "randomDenominations"
+);
+const countdownDisplay = document.getElementById("countdown");
+const canvas = document.getElementById("my-canvas");
 const denominations = [1, 2, 5, 10, 20, 50, 100, 200, 500];
-const weights = [0.3, 0.25, 0.2, 0.1, 0.05, 0.05, 0.025, 0.015, 0.01];
+const weights = [0.1, 0.1, 0.1, 0.2, 0.22, 0.24, 0.26, 0.06, 0.04];
+// Initialize Firebase
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDJWGGsB5DGLZs9t6L7CqJu18v8hfwVPQc",
+  authDomain: "lucky-money-2a779.firebaseapp.com",
+  databaseURL:
+    "https://lucky-money-2a779-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "lucky-money-2a779",
+  storageBucket: "lucky-money-2a779.firebasestorage.app",
+  messagingSenderId: "283487085115",
+  appId: "1:283487085115:web:54aaecbcc849cbd4a3f21e",
+  measurementId: "G-22BWMQNXNG",
+};
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
 function weightedRandom(items, weights) {
-    let totalWeight = weights.reduce((sum, w) => sum + w, 0);
-    let randomNum = Math.random() * totalWeight;
-    let weightSum = 0;
+  let totalWeight = weights.reduce((sum, w) => sum + w, 0);
+  let randomNum = Math.random() * totalWeight;
+  let weightSum = 0;
 
-    for (let i = 0; i < items.length; i++) {
-        weightSum += weights[i];
-        if (randomNum <= weightSum) {
-            return items[i];
-        }
+  for (let i = 0; i < items.length; i++) {
+    weightSum += weights[i];
+    if (randomNum <= weightSum) {
+      return items[i];
     }
-    return items[items.length - 1];
-}
-
- function findNearestDenomination(apiNumber) {
-    let closest = denominations[0];
-    let minDiff = Math.abs(apiNumber - closest);
-
-    for (let i = 1; i < denominations.length; i++) {
-        const diff = Math.abs(apiNumber - denominations[i]);
-        if (diff < minDiff) {
-            minDiff = diff;
-            closest = denominations[i];
-        }
-    }
-    return closest;
+  }
+  return items[items.length - 1];
 }
 
 function showRandomDenominations(duration) {
-   randomDenominationsDisplay.style.display = 'block';
-   let startTime = performance.now();
-   const interval = setInterval(() => {
-     if (performance.now() - startTime > duration) {
-       clearInterval(interval);
-        randomDenominationsDisplay.style.display = 'none';
-      } else {
-        const randomIndex = Math.floor(Math.random() * denominations.length);
-         const randomDenomination = denominations[randomIndex];
-       randomDenominationsDisplay.innerHTML = `<span>${randomDenomination}</span>`;
-      }
-    }, 100);
+  const startTime = performance.now();
+  const interval = setInterval(() => {
+    if (performance.now() - startTime > duration) {
+      clearInterval(interval);
+    } else {
+      const randomDenomination = weightedRandom(denominations, weights);
+      resultDisplay.textContent = `${randomDenomination.toLocaleString(
+        "en-US"
+      )}.000 VNĐ`;
+    }
+  }, 50);
 }
-  function updateCountdown() {
-  const tet2025 = new Date('2025-01-29T00:00:00');
+
+function updateCountdown() {
+  const tet2025 = new Date("2025-01-29T00:00:00");
   const now = new Date();
   const diff = tet2025 - now;
 
-    if (diff > 0) {
-      const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+  if (diff > 0) {
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
 
-      countdownDisplay.textContent = `Còn ${days} ngày ${hours} giờ ${minutes} phút ${seconds} giây đến Tết Nguyên Đán 2025`;
-    } else {
-         countdownDisplay.textContent = 'Chúc Mừng Năm Mới!';
-    }
+    countdownDisplay.innerHTML = `Còn ${days} ngày ${hours} giờ ${minutes} phút ${seconds} giây <br>là đến Tết Nguyên Đán 2025`;
+  } else {
+    countdownDisplay.textContent = "Chúc Mừng Năm Mới!";
   }
-  updateCountdown();
-    setInterval(updateCountdown, 1000);
+}
+updateCountdown();
+setInterval(updateCountdown, 1000);
 
+function fireConfetti() {
+  var count = 200;
+  var defaults = {
+    origin: { y: 0.7 },
+  };
 
-randomButton.addEventListener('click', async () => {
-    resultDisplay.textContent = '';
-    showRandomDenominations(5000);
-  
-    setTimeout(async () => {
-        try {
-            const response = await fetch('https://api.random.org/json-rpc/4/invoke', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    "jsonrpc": "2.0",
-                    "method": "generateIntegers",
-                    "params": {
-                        "apiKey": apiKey,
-                        "n": 1,
-                        "min": 1,
-                        "max": 100
-                    },
-                    "id": 1
-                })
-            });
+  function fire(particleRatio, opts) {
+    confetti({
+      ...defaults,
+      ...opts,
+      particleCount: Math.floor(count * particleRatio),
+      canvas: canvas,
+    });
+  }
 
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
+  fire(0.25, {
+    spread: 26,
+    startVelocity: 55,
+  });
+  fire(0.2, {
+    spread: 60,
+  });
+  fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8,
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2,
+  });
+  fire(0.1, {
+    spread: 120,
+    startVelocity: 45,
+  });
+}
 
-            const data = await response.json();
-            if (data.result && data.result.random && data.result.random.data && data.result.random.data.length > 0) {
-               const apiNumber = data.result.random.data[0];
-               const nearestDenomination = findNearestDenomination(apiNumber);
-                  resultDisplay.textContent = `${nearestDenomination} VNĐ`;
-            } else {
-                throw new Error('Invalid response from API');
-            }
-        } catch (error) {
-            console.error('Error fetching random number:', error);
-            resultDisplay.textContent = 'Lỗi khi lấy lộc. Vui lòng thử lại.';
+randomButton.addEventListener("click", async () => {
+    const userId = getUserId();
+    const today = new Date().toLocaleDateString();
+    const userRef = database.ref(`users/${userId}/${today}`);
+
+    userRef.once('value', snapshot => {
+        if (snapshot.exists()) {
+            // User đã xem hôm nay
+            resultDisplay.textContent = 'Bạn đã nhận lộc hôm nay rồi, hãy quay lại vào ngày mai nha!';
+        } else {
+            const randomDenomination = weightedRandom(denominations, weights);
+            resultDisplay.textContent = `${randomDenomination.toLocaleString("en-US")}.000 VNĐ`;
+            fireConfetti();
+            userRef.set(randomDenomination);
         }
-      },5000);
+    });
 });
+
+
+function getUserId() {
+  // Bạn có thể dùng cookie, localStorage, hoặc bất cứ cách nào để tạo userId
+  // Dưới đây là cách dùng localStorage. Bạn có thể thay thế
+  let userId = localStorage.getItem("userId");
+  if (!userId) {
+    userId =
+      Math.random().toString(36).substring(2, 15) +
+      Math.random().toString(36).substring(2, 15);
+    localStorage.setItem("userId", userId);
+  }
+  return userId;
+}
